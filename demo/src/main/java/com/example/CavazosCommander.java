5d770353644f0200d35ac3435bf560a69bfcce23
Saket Pathak <99856959+psaket0@users.noreply.github.com>
2023-04-19 01:10:26 -0700

    test 6

--- demo/src/main/java/com/example/CavazosCommander.java
+++ demo/src/main/java/com/example/CavazosCommander.java
@@ -1,95 +1,80 @@
 package com.example;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import java.util.Stack;
 import java.util.Scanner;
 
-public class App {
-
-    private static final List<String> COMMANDS = List.of(
+public class CavazosCommander {
+    private static final List<String> COMMANDS = Arrays.asList(
             "About Face", "Change Step", "Close Ranks", "Column Right", "Count Off", "Cover", "Eyes Right",
             "Fall In", "Fall Out", "Flight Attention", "Flight Halt", "Forward March", "Left Face", "Left Flank",
             "Open Ranks", "Order Arms", "Parade Rest", "Present Arms", "Present Arms  ", "Ready Front",
             "Report In", "Report Out", "Right Face", "Right Flank", "Right Step, March", "To the Rear, March");
 
-    private List<String> commands = new ArrayList<>();
-    private List<String> undone = new ArrayList<>();
-    private Random random = new Random();
-    private Scanner scanner = new Scanner(System.in);
+    private static final Random random = new Random();
+    private static final Stack<String> commandHistory = new Stack<>();
+    private static final Stack<String> redoHistory = new Stack<>();
 
     public static void main(String[] args) {
-        App commander = new App();
-        commander.run();
-    }
-
-    private void run() {
-        while (true) {
-            System.out.println("--------General Cavazos Commander App------------");
-            System.out.println("i      issue a command");
-            System.out.println("l      list commands");
-            System.out.println("u      undo last command");
-            System.out.println("r      redo last command");
-            System.out.println("q      quit");
-            System.out.println("-------------------------------------------------");
+        System.out.println("-----------------------------------------------------\n");
+        System.out.println("General Cavazos Commander App\n");
+        System.out.println("-----------------------------------------------------\n");
+        printMenu();
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        do {
             System.out.print("Enter a command: ");
-            String command = scanner.nextLine();
-            switch (command) {
+            input = scanner.nextLine().toLowerCase();
+            switch (input) {
                 case "i":
-                    System.out.println("Issuing command...");
-                    System.out.println(issueCommand());
+                    String command = COMMANDS.get(random.nextInt(COMMANDS.size()));
+                    System.out.println("Issuing command: " + command);
+                    commandHistory.push(command);
                     break;
                 case "l":
-                    System.out.println("Listing commands...");
-                    listCommands();
+                    System.out.println("List of commands:");
+                    for (String c : COMMANDS) {
+                        System.out.println("- " + c);
+                    }
                     break;
                 case "u":
-                    System.out.println("Undoing last command...");
-                    undoCommand();
+                    if (!commandHistory.isEmpty()) {
+                        String lastCommand = commandHistory.pop();
+                        redoHistory.push(lastCommand);
+                        System.out.println("Undoing command: " + lastCommand);
+                    } else {
+                        System.out.println("No commands to undo.");
+                    }
                     break;
                 case "r":
-                    System.out.println("Redoing last command...");
-                    redoCommand();
+                    if (!redoHistory.isEmpty()) {
+                        String lastUndoneCommand = redoHistory.pop();
+                        commandHistory.push(lastUndoneCommand);
+                        System.out.println("Redoing command: " + lastUndoneCommand);
+                    } else {
+                        System.out.println("No commands to redo.");
+                    }
                     break;
                 case "q":
                     System.out.println("Quitting...");
-                    scanner.close();
-                    return;
+                    break;
                 default:
-                    System.out.println("Invalid command: " + command);
+                    System.out.println("Invalid command. Please try again.");
+                    break;
             }
-        }
+        } while (!input.equals("q"));
     }
 
-    private String issueCommand() {
-        String command = COMMANDS.get(random.nextInt(COMMANDS.size()));
-        commands.add(command);
-        return command;
-    }
-
-    private void listCommands() {
-        for (int i = 0; i < commands.size(); i++) {
-            System.out.printf("%d. %s\n", i + 1, commands.get(i));
-        }
-    }
-
-    private void undoCommand() {
-        if (!commands.isEmpty()) {
-            String command = commands.remove(commands.size() - 1);
-            undone.add(command);
-            System.out.println("Undid command: " + command);
-        } else {
-            System.out.println("No commands to undo");
-        }
-    }
-
-    private void redoCommand() {
-        if (!undone.isEmpty()) {
-            String command = undone.remove(undone.size() - 1);
-            commands.add(command);
-            System.out.println("Redid command: " + command);
-        } else {
-            System.out.println("No commands to redo");
-        }
+    private static void printMenu() {
+        System.out.println("i - issue a command");
+        System.out.println("l - list all commands");
+        System.out.println("u - undo last command");
+        System.out.println("r - redo last command");
+        System.out.println("q - quit");
+        System.out.println("-----------------------------------------------------\n");
+        System.out.println();
     }
 }
